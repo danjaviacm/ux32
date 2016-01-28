1bdb2f42f9a2f9ac1eeb10cd8421f9ddff050144
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2016-01-28 14:17:28 -0500

    Cambio de constantes en ux32

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,19 +1,14 @@
-let Env = 'dev';
+let ENV = 'dev';
 let BASE_URL = 'http://localhost:8000/';
-let key_pusher = '5ad5c09fbf6f2ffe36f6';
+let KEY_PUSHER = '5ad5c09fbf6f2ffe36f6';
 let IMG = 'http://segdig1.s3.amazonaws.com';
 
 if(window.location.host == 'seguros.comparamejor.com'){
-    Env = 'prd';
+    ENV = 'prd';
     BASE_URL = 'https://seguros.comparamejor.com/';
-    key_pusher = '3173debe12cb90da0c9f';
+    KEY_PUSHER = '3173debe12cb90da0c9f';
     IMG = ''
 }
 
 
-export default {
-    BASE_URL: BASE_URL,
-    ENV: Env,
-    KEY_PUSHER: key_pusher,
-    IMG: IMG
-}
+export { BASE_URL, ENV, KEY_PUSHER, IMG }
